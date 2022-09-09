7c9bb6aade75c61e5969c3ddd3f27b445d6e9875
damorenor <damorenor@unal.edu.co>
2022-09-08 20:05:56 -0500

    cards pictures styled but with basic hover

--- src/components/Secciones/SeccionesCard.js
+++ src/components/Secciones/SeccionesCard.js
@@ -2,11 +2,12 @@ import React from "react";
 
 const SeccionesCard = ({ title, bg, len }) =>{
     const card_h = 100/len;
+    const card_bg ='linear-gradient(rgba(0,0,0,.4),rgba(0,0,0,.4)),url('+require(`../../assets/${bg}`)+')';
     return(
         <div
-            className="flex justify-center items-center ma3 seccard"
+            className="flex f3 dim justify-center items-center ma3 seccard pointer"
             style={{
-                backgroundImage: 'url('+require(`../../assets/${bg}`)+')',
+                backgroundImage: card_bg,
                 height: `${card_h}%`,
             }}
         >
