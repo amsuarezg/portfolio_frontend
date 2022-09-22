0b5bb9eb037e36bee1f22048459e5d3c54ab6b0a
damorenor <damorenor@unal.edu.co>
2022-09-22 16:25:49 -0500

    quick hover fix, still bad af

--- src/components/Secciones/SeccionesCard.js
+++ src/components/Secciones/SeccionesCard.js
@@ -1,17 +1,19 @@
 import React from "react";
 
 const SeccionesCard = ({ title, bg, len }) =>{
-    const card_h = 100/len;
+    const card_h = (100/len)-7;
     const card_bg ='linear-gradient(rgba(0,0,0,.4),rgba(0,0,0,.4)),url('+require(`../../assets/${bg}`)+')';
     return(
-        <div
-            className="flex f3 dim justify-center items-center ma3 seccard pointer"
-            style={{
-                backgroundImage: card_bg,
-                height: `${card_h}%`,
-            }}
-        >
-            {title}
+        <div className="seccard_out ma3">
+            <div
+                className="flex f3 secdim justify-center items-center seccard pointer"
+                style={{
+                    backgroundImage: card_bg,
+                    height: `${card_h}vh`,
+                }}
+            >
+                {title}
+            </div>
         </div>
     )
 }
